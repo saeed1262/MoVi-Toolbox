ff564bfec141d0e9c2f48e2aa1f0edd5f0d50360
saeed126 <saeed.ghorbani1262@gmail.com>
2020-03-06 10:43:55 -0500

    Tutorials added

--- MoCap/display_overlay.m
+++ MoCap/display_overlay.m
@@ -1,4 +1,4 @@
-function display_overlay(jointsLocation, videoName, cameraParams, rotationMatrix, translationVector, verts)
+function display_overlay(jointsLocation, videoName, cameraParams, rotationMatrix, translationVector, faces, verts)
 % DISPLAY_OVERLAY - Displays joints and mesh overlaid on video
 % 
 %
@@ -35,7 +35,7 @@ else
     disp_projections(imagePoints_joints, videoName)
 end
 
-function disp_projections(imagePoints, videoName, faces, imagePoints_2)
+function disp_projections(imagePoints, videoName, faces, verts)
 % The function takes 2D projected points and displays it over the corresponding
 % video
 vidObj = VideoReader(videoName);
@@ -51,11 +51,12 @@ while hasFrame(vidObj)
     image(vidFrame, 'Parent', currAxes);
     hold on
     imagePoints = double(imagePoints);
-    plot(imagePoints(counter,:,1),imagePoints(counter,:,2),'go','MarkerSize',5,'LineWidth',1,'MarkerFaceColor',[0, 255, 0]/255)
+    
     if exist('verts')
-        TR = triangulation(faces+1,squeeze(imagePoints_2(counter,:,:)));
+        TR = triangulation(double(faces+1),squeeze(verts(counter,:,:)));
         triplot(TR,'Color',[0.8500 0.3250 0.0980], 'LineWidth', 0.1);
     end
+    plot(imagePoints(counter,:,1),imagePoints(counter,:,2),'go','MarkerSize',5,'LineWidth',1,'MarkerFaceColor',[0, 255, 0]/255)
     hold off
     pause(1/vidObj.FrameRate);
     counter = counter + 1;
